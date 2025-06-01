77884a30ebd7f752da36a537f458cff9f65af976
Laith Bahodi <70682032+hydrobeam@users.noreply.github.com>
2025-06-01 10:27:36 -0400

    feat(org-wasm): site improvements (#9)
    * feat(org-wasm): site improvements
    
    - github icon topright
    - remove text blurb below editor
    - use height:100vh to have editor fill screen
      - + overflow-hidden on parent to make it not expand
    - formatting
    - minor padding changes
    
    * drop minimizer, causing problems

--- crates/org-wasm/webpack.config.js
+++ crates/org-wasm/webpack.config.js
@@ -8,7 +8,6 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 import HtmlBundlerPlugin from "html-bundler-webpack-plugin";
 import TerserPlugin from 'terser-webpack-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-import HtmlMinimizerPlugin from 'html-minimizer-webpack-plugin';
 
 
 
@@ -60,11 +59,6 @@ export default {
   },
   optimization: {
     minimizer: [
-      new HtmlMinimizerPlugin(
-        {
-          minify: HtmlMinimizerPlugin.minifyHtmlNode
-        }
-      ),
       // mirroring the default webpack config
       // adding the other minimizers causes this one to not activate,
       // so be explicit about using it
